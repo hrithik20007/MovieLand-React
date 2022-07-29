31fd96de4565151363334cdf79c4bfa30b34acae
hrithik20007 <darthvader20137@gmail.com>
2022-07-29 15:21:22 +0530

    API fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { useEffect } from 'react'
 
 const URL='http://www.omdbapi.com?apikey=53da6a14'
 
-
 const App = () =>{
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${URL}&s=${title}`) 
+        const data = await response.json()
+        console.log(data.Search)
+    }
+    
+    useEffect(()=>{
+        searchMovies('Batman')
+    },[])
+
     return(
         <h1>Hello</h1>
     );
